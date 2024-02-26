2392a96d9c10eb13f9388ae683e8bc9f4399540d
shruti-7garg <121709168+shruti-7garg@users.noreply.github.com>
2024-02-26 09:55:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
-# pip install speechrecognition
-from typing import Any
+# pip install following packages 
 
-import speech_recognition as sr
-#import distutils.version
-
-# install package pywin32 from settings to make system speak
+import random
+import pyttsx3
 import win32com.client
-
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
-
-def takeCommand():
-    r= sr.Recognizer()
-    with sr.Microphone() as source:
-        r.pause_threshold= 1
-        audio = r.listen(source)
-        try:
-            query= r.recognize_google_cloud(audio,language="en-in")
-            print(f"User said: {query}")
-            return query
-        except Exception as e:
-            return "Some error occured"
-
-
-print("Enter the word")
-s= input()
-speaker.Speak(s)
-print("Listening...")
-while True:
-           text= takeCommand()
-           speaker.Speak(text)
-
- # speaker.speak
-
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+import speech_recognition as sr
 
